2c0a538620fe8e4b852dcaa7d6ca15b743c1ea3b
ZhaoXiaodong <zhaoxd@wifi.com>
2018-09-10 19:53:57 +0800

    feat: add response filter

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,23 @@ import mock from './mock';
 
 const req = request({
   timeout: 5000
+}, {
+  filterResponse(data) {
+    if (data.status === 0) {
+      return Promise.resolve(data.data);
+    }
+
+    return Promise.reject(new Error(data.message));
+  },
+  beautifyError(url, err) {
+    return new Error(`Request url ${url} error, message: ${err.message}`);
+  }
 });
 
 export function getUser() {
   return req.get(USER);
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   mock();  
 }
