114dcedb3035908985f2b6b362721f657debbf68
ZhaoXiaodong <zhaoxd@wifi.com>
2018-09-12 16:33:14 +0800

    feat: add touchstart event to fix :active

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect, NavLink, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 import cns from 'classnames';
@@ -20,39 +20,49 @@ const NavItem = ({ label, to, activeOnlyWhenExact }) => (
   </li>
 );
 
-const Container = () => (
-  <div className={cns('container', style.container)}>
-    <header className={style.header}>
-      <h1>React Redux (Ultimate)</h1>
-    </header>
-    <div className={style.content}>
-      <Switch>
-        <Redirect
-          from="/"
-          to="/counter"
-          exact
-        />
-        <Route
-          path="/counter"
-          component={Counter}
-        />
-        <Route
-          path="/user"
-          component={User}
-        />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-    <footer className={style.footer}>
-      <nav className={style.nav}>
-        <ul>
-          <NavItem to="/counter" label="Counter" />
-          <NavItem to="/user" label="User" />
-        </ul>
-      </nav>
-    </footer>
-  </div>
-);
+class Container extends Component {
+  componentDidMount() {
+
+    // for element:active
+    document.body.addEventListener('touchstart', () => {});
+  }
+
+  render() {
+    return (
+      <div className={cns('container', style.container)}>
+        <header className={style.header}>
+          <h1>React Redux (Ultimate)</h1>
+        </header>
+        <div className={style.content}>
+          <Switch>
+            <Redirect
+              from="/"
+              to="/counter"
+              exact
+            />
+            <Route
+              path="/counter"
+              component={Counter}
+            />
+            <Route
+              path="/user"
+              component={User}
+            />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+        <footer className={style.footer}>
+          <nav className={style.nav}>
+            <ul>
+              <NavItem to="/counter" label="Counter" />
+              <NavItem to="/user" label="User" />
+            </ul>
+          </nav>
+        </footer>
+      </div>
+    );
+  }
+} 
 
 const App = () => (
   <Router basename={APP_BASE_URL}>
