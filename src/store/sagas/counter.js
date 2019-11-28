ae87c2ac989e353edd1b3906e434ace87cd8de2d
Nicolas <zhaoxd@zenmen.com>
2019-11-28 15:44:02 +0800

    feat: delay to an effect

--- src/store/sagas/counter.js
+++ src/store/sagas/counter.js
@@ -1,9 +1,8 @@
-import { delay } from 'redux-saga';
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest, delay } from 'redux-saga/effects';
 import { actions } from '../reducers/counter';
 
 export function* incrementAsync({ payload }) {
-  yield call(delay, 1000);
+  yield delay(1000);
   yield put(actions.increment(payload.amount));
 }
 
